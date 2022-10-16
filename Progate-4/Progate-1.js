2b93eed4d827b1200b042025fabaca795243156d
Takahisa99 <takahi915@gmail.com>
2022-10-16 07:30:22 +0000

    Progate更新

---
+++ Progate-4/Progate-1.js
@@ -0,0 +1,22 @@
+const user = {
+  name: "にんじゃわんこ",
+  greet: () => {
+    console.log("こんにちは");
+  }
+};
+user.greet();
+
+
+// 定数animalを定義してください
+const animal = {
+
+// animalのnameプロパティの値を出力してください
+  name: "レオ",
+  age: 3,
+  greet: () => {
+    console.log("こんにちは");
+  }
+}
+// animalのgreetプロパティの関数を実行してください
+console.log(animal.name);
+animal.greet();
